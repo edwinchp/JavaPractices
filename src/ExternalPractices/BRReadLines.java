b530cd7c196065bff8ec1adef26ab3f4b7b366ad
Edwin Chi Pineiro <chipineiro@gmail.com>
2019-04-23 22:55:50 -0500

    ReadingLines class added and package renamed to ExternalPractices

---
+++ src/ExternalPractices/BRReadLines.java
@@ -0,0 +1,34 @@
+package ExternalPractices;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class BRReadLines {
+
+    public static void main(String args[]) throws Exception{
+
+        readLines("HA");
+        readLines("STOP");
+
+    }
+
+    private static void readLines(String wordToStop) throws Exception{
+        InputStream inputStream = System.in;
+        InputStreamReader streamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(streamReader);
+
+        String inputText;
+
+        System.out.println("Enter \"" + wordToStop + "\" to stop the loop :)");
+        do{
+            inputText = bufferedReader.readLine();
+            if(!wordToStop.equals(inputText)){
+                System.out.println("You entered: \"" + inputText + "\" and it's wrong, try again:");
+            }
+
+        }while (!inputText.equals(wordToStop));
+
+        System.out.println("Finally :D");
+    }
+}
