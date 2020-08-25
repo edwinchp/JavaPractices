831bb30c5d1bd13569b5a4f4aa7ce61171cc6e7b
Edwin Chi Pineiro <chipineiro@gmail.com>
2020-08-24 21:30:42 -0500

    Implementing an interface

---
+++ src/exam_guide/a4_interfaces/Ball.java
@@ -0,0 +1,17 @@
+package exam_guide.a4_interfaces;
+
+public class Ball implements Bounceable {
+
+    /*
+        If we want to implement Bounceable, we need to implement
+        the method within Bounceable because it is an Interface.
+     */
+
+    public void bounce(){
+
+    }
+
+    public void setBounceFactor(int bounceFactor) {
+
+    }
+}
---
+++ src/exam_guide/a4_interfaces/Bounceable.java
@@ -0,0 +1,10 @@
+package exam_guide.a4_interfaces;
+
+/**
+ * All the concrete classes that implements this Interface must implement the methods
+ * that are defined here.
+ */
+public interface Bounceable {
+    public void bounce();
+    public void setBounceFactor(int bounceFactor);
+}
