2b3d27645e10b002620f12f4c8cd9a77da491514
Edwin Chi Pineiro <chipineiro@gmail.com>
2020-08-24 22:21:31 -0500

    BallAbstract and BeachBall

---
+++ src/exam_guide/a4_interfaces/BallAbstract.java
@@ -0,0 +1,9 @@
+package exam_guide.a4_interfaces;
+
+public abstract class BallAbstract implements Bounceable {
+
+    /*
+        Since this is an abstract class, we don't need to implements
+        the methods within Bounceable.
+     */
+}
---
+++ src/exam_guide/a4_interfaces/BeachBall.java
@@ -0,0 +1,12 @@
+package exam_guide.a4_interfaces;
+
+public class BeachBall extends BallAbstract{
+    /*
+        We need to implement the methods from Bounceable because
+        BallAbstract is implement it.
+     */
+
+    public void bounce(){}
+
+    public void setBounceFactor(int bounceFactor) {}
+}
